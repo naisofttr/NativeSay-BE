a3f48a2dda8ca90af67df526c402f11d3c256334
Ali Taş <naisofttr@gmail.com>
2025-01-17 17:17:34 +0300

    jwtToken added 100d expireIn for generate

--- src/services/JwtTokenServices/generateJwtToken.ts
+++ src/services/JwtTokenServices/generateJwtToken.ts
@@ -2,5 +2,5 @@ import jwt from 'jsonwebtoken';
 
 export const generateJwtToken = async (payload: object): Promise<string> => {
     const secretKey = process.env.JWT_SECRET || 'your_jwt_secret';
-    return jwt.sign(payload, secretKey);
+    return jwt.sign(payload, secretKey, { expiresIn: '100d' });
 };
